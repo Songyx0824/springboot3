753ae8a9a71dfcdf98a71fd02686c1e1c3b6363b
Songyx0824 <18045025768@163.com>
2024-07-12 23:04:13 +0800

    SpringBoot3

---
+++ boot3-01-demo/src/main/java/com/spring/boot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.spring.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+
+
+    @GetMapping("/hello")
+    public String HelloWordController(){
+
+        return "Hello, Spring Boot 3";
+    }
+}
