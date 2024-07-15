6de94029593db315d1fd55eac29e746be904b5f9
Songyx0824 <18045025768@163.com>
2024-07-15 19:52:40 +0800

    SpringBoot3

--- boot3-02-demo/src/main/java/com/srpingboot/song/boot/MainApplication.java
+++ boot3-02-demo/src/main/java/com/srpingboot/song/boot/MainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot302DemoApplication {
+public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Boot302DemoApplication.class, args);
-    }
 
+        SpringApplication.run(MainApplication.class,args);
+    }
 }
